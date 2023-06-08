150c6beeab3a9f9df1a7b4d97e892f69a5813741
dayanidi <93707264+dayanid@users.noreply.github.com>
2023-06-08 11:16:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,2 @@
-const express = require('express');
 console.log("its okay");
-// Create an instance of the Express application
-const app = express();
 
-// Define a route
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
-
-// Start the server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
