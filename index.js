57f2124ce04a18b6a083873625aa26f8de717b2e
dayanidi <93707264+dayanid@users.noreply.github.com>
2023-06-26 11:26:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const secretValue = process.env.HELLO;
-console.log("Secret value",process);
-// Use the secret value in your code
-console.log("Secret value:", process.env.KEY);
+
+console.log("This is node code.");
 
