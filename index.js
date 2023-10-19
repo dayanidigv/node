41b9f414af7353abc039aac197ae7fead95d949a
dayanidi <93707264+dayanidigv@users.noreply.github.com>
2023-10-19 11:22:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+// Import required modules
+const express = require('express');
 
-console.log("This is node code.");
+// Create an Express application
+const app = express();
+
+// Sample data - list of users
+const users = [
+  { id: 1, name: 'Alice' },
+  { id: 2, name: 'Bob' },
+  { id: 3, name: 'Charlie' },
+];
+
+// Define a route for the API endpoint
+app.get('/api/users', (req, res) => {
+  // Return the list of users as JSON
+  res.json(users);
+});
+console.log("This is node api  code.");
+// Start the server and listen on port 3000
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
 
